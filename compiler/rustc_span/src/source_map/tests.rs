19a33d1a20ee894fcb087b6e732e57beee39a847
Camille GILLOT <gillot.camille@gmail.com>
2023-09-05 16:48:34 +0000

    Do not insert duplicate file.
    Co-authored-by: John Kåre Alsaker <john.kare.alsaker@gmail.com>

--- compiler/rustc_span/src/source_map/tests.rs
+++ compiler/rustc_span/src/source_map/tests.rs
@@ -235,7 +235,7 @@ fn t10() {
         lines,
         multibyte_chars,
         normalized_pos,
-        stable_id,
+        stable_id: _,
         ..
     } = (*src_file).clone();
 
@@ -243,7 +243,9 @@ fn t10() {
         name,
         src_hash,
         checksum_hash,
-        stable_id,
+        StableSourceFileId::from_filename_in_current_crate(
+            &PathBuf::from("imported-blork.rs").into(),
+        ),
         normalized_source_len.to_u32(),
         unnormalized_source_len,
         CrateNum::ZERO,
